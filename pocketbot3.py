e863102c86f77e67f500b8c27248fca8d7ce56d4
Arnav Patel <Arnav Patel>
2017-05-25 01:04:22 -0400

    ...

--- pocketbot3.py
+++ pocketbot3.py
@@ -461,6 +461,7 @@ def randomL(bot,update):
     data = json.loads(x)
     dataValues = data.values()
     articlesList = dataValues[2]
+    print type (articleList)
     print "++++++++++++++++++++++++"
     print articlesList
     print "++++++++++++++++++++++++"
